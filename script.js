8ec6266915e5f7ff52c0e460902e7358892b4934
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-09-23 16:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const checkboxes = document.querySelectorAll('.toggle');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+        
+        if (checkedCheckboxes.length > 2) {
+            // Uncheck the last checked checkbox
+            checkedCheckboxes[0].checked = false;
+        }
+    });
+});
